57cdafdf4bc57210bc9ca7df6745ab708cd961b8
barangurbulak <barangurbulak99@gmail.com>
2025-03-02 15:18:04 +0300

    bg
    bg

--- 1.Hafta/Eğitim Sonrası/deneme2.py
+++ 1.Hafta/Eğitim Sonrası/deneme2.py
@@ -1,1 +1,8 @@
-print("This is my homework")
+a = 1
+b = 5
+if a<b:
+    print(a<b)
+elif a>b:
+    print("a b den küçük")
+else:
+    print("hello")
--- 1.Hafta/Eğitim Öncesi/deneme.py
+++ 1.Hafta/Eğitim Öncesi/deneme.py
@@ -1,1 +1,8 @@
-print("hello")
+a = 1
+b = 5
+if a<b:
+    print(a<b)
+elif a>b:
+    print("a b den küçük")
+else:
+    print("hello")
